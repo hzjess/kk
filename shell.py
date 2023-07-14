b8af6eed02704252183275100172df63e9767235
hzjess <139531293+hzjess@users.noreply.github.com>
2023-07-14 23:02:40 +0800

    Add files via upload

---
+++ shell.py
@@ -0,0 +1,14 @@
+# import time
+# import os
+# publish_Time = "2018年10月10日"
+# array = time.strptime(publish_Time, u"%Y年%m月%d日")
+# publishTime = time.strftime("%Y-%m-%d", array)
+#
+#
+# a=os.path.getsize('D:\BaiduNetdiskDownload\阿凡达：水之道.Avatar.The.Way.Of.Water.2022.HD1080P.X264.AAC.English.CHS-ENG.BDYS\Avatar.The.Way.Of.Water.2022.HD1080P.X264.AAC.English.CHS-ENG.BDYS.mp4')
+# print(a)
+# a,b = 'kk'
+# print(a)
+# print(os.SEEK_SET)
+
+from dataset.mnist import load_
